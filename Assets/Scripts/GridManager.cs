012871551486f247bbd02e4cb7e18963bed7c197
Jesal Vadgama <jesalvadgama@gmail.com>
2025-04-22 22:13:59 +0100

    code: cannot collide with obstacles map position fix

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,43 +1,60 @@
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField] private int _width, _height;
-    [SerializeField] private Tile _tilePrefab;
-    [SerializeField] private Transform _cam;
-    [SerializeField] private PlayerController _playerPrefab;
+    [SerializeField] private int width, height;
+    [SerializeField] private Tile tilePrefab;
+    [SerializeField] private Transform cam;
+    [SerializeField] private PlayerController playerPrefab;
+    [SerializeField] private Tilemap obstacleTilemap;
 
-    private PlayerController _player;
-    private readonly List<Tile> _tiles = new List<Tile>();
+    private PlayerController player;
+    private readonly List<Tile> tiles = new();
+    private readonly HashSet<Vector2Int> blockedCells = new();
+
+    void Awake() 
+    { 
+        obstacleTilemap ??= GameObject.Find("TilemapObstacles")?.GetComponent<Tilemap>(); 
+        if (obstacleTilemap != null) CacheObstacles();
+    }
+
+    void Start()
+    { 
+        GenerateGrid(); player = Instantiate(playerPrefab); 
+        player.Init(new Vector2Int(3, 4), this); 
+        cam.position = new Vector3(width / 2f - .5f, height / 2f - .5f, -10); 
+    }
+
+    private void CacheObstacles()
+    {
+        var bounds = obstacleTilemap.cellBounds;
+        foreach (var cell in bounds.allPositionsWithin)
+            if (obstacleTilemap.GetTile(cell) != null)
+            {
+                var w = obstacleTilemap.CellToWorld(cell) + obstacleTilemap.tileAnchor;
+                var gp = new Vector2Int(Mathf.RoundToInt(w.x), Mathf.RoundToInt(w.y));
+                blockedCells.Add(gp);
+                Debug.Log($"Blocked grid cell: {gp.x}, {gp.y}");
+            }
+    }
+
+    private void GenerateGrid()
+    {
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
+            {
+                var t = Instantiate(tilePrefab, new Vector3(x, y), quaternion.identity);
+                t.name = $"Tile {x} {y}";
+                t.Init((x % 2 == 0) ^ (y % 2 == 0), new Vector2Int(x, y));
+                tiles.Add(t);
+            }
+    }
 
     public bool IsPositionValid(Vector2Int pos) =>
-        pos.x >= 0 && pos.x < _width && pos.y >= 0 && pos.y < _height;
+        pos.x >= 0 && pos.x < width && pos.y >= 0 && pos.y < height && !blockedCells.Contains(pos);
 
-    void Start() =>
-        GenerateGrid();
-
-    void GenerateGrid()
-    {
-        for (int x = 0; x < _width; x++)
-            for (int y = 0; y < _height; y++)
-            {
-                var spawned = Instantiate(_tilePrefab, new Vector3(x, y), quaternion.identity);
-                spawned.name = $"Tile {x} {y}";
-                var isOffset = (x % 2 == 0) ^ (y % 2 == 0);
-                spawned.Init(isOffset, new Vector2Int(x, y));
-                _tiles.Add(spawned);
-            }
-
-        _player = Instantiate(_playerPrefab);
-        _player.Init(new Vector2Int(3, 4), this);
-        _cam.position = new Vector3(_width/2f - .5f, _height/2f - .5f, -10);
-    }
-
-    public void ClearHighlights()
-    {
-        foreach (var t in _tiles)
-            t.HideHighlight();
-    }
+    public void ClearHighlights() => tiles.ForEach(t => t.HideHighlight());
 }
