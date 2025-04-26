3379f376e14858b782b887020b2622cd3411eda1
Jesal Vadgama <jesalvadgama@gmail.com>
2025-04-26 18:55:16 +0100

    sprites: level 4 design complete

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,14 +6,14 @@ public class EnemySpawner : MonoBehaviour
     Vector3?[,] spawnPoints = {{null, null}, 
                                {null, null},
                                {new Vector3(10, 3, -1), new Vector3(8, 2, -1)},
-                               {new Vector3(13, 2, -1), new Vector3(10, 2, -1)},
+                               {new Vector3(6, 8, -1), new Vector3(14, 4, -1)},
                                {new Vector3(13, 2, -1), new Vector3(10, 2, -1)}};
 
     // -1. x 0.> 1.< 2.^ 3.v                           
     int[,] modes = {{-1, -1}, // level 1
                    {-1, -1}, // level 2
                    {0, 1}, // level  3
-                   {-1, -1}, // level 4
+                   {3, 1}, // level 4
                    {-1, -1}}; // level 5
 
     void Start()
